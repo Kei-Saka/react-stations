3d38ab5cbc8dda73929d6224fbe469ef8204bf00
kei.sakanishi@gmail.com <kei.sakanishi@gmail.com>
2022-02-08 14:41:49 +0900

    delete 'e.preventDefault();'

--- src/App.js
+++ src/App.js
@@ -10,11 +10,17 @@ import './App.css'
  */
 export const App = () => {
   const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/maltese/n02085936_2422.jpg');
+  function handleClick(e) {
+    // e.preventDefault();
+    console.log('changeImage');
+    setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg');
+  }
   return (
     <div>
       <header>Dogアプリ</header>
       <p>犬の画像を表示するサイトです。</p>
       <img src={dogUrl} />
+      <button onClick={handleClick}>更新</button>
     </div>
   )
 }
