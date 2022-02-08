88b5165c01ee51b28e271f16cb85f6839f0139c9
kei.sakanishi@gmail.com <kei.sakanishi@gmail.com>
2022-02-08 20:25:14 +0900

    station6

--- src/App.js
+++ src/App.js
@@ -10,10 +10,31 @@ import './App.css'
  */
 export const App = () => {
   const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/maltese/n02085936_2422.jpg');
-  function handleClick(e) {
-    // e.preventDefault();
-    console.log('changeImage');
-    setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg');
+  // function handleClick() {
+  //   // e.preventDefault();
+  //   console.log('changeImage');
+  //   // setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg');
+  //   fetch("https://dog.ceo/api/breeds/image/random").then(function(response) {
+  //     setDogUrl(response);
+  //     console.log(dogUrl);
+  //   });
+  // }
+
+  /* 理解しつつも最後はSlack頼りになってしまった */
+  const handleClick = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      /* 本文のテキストをJSONとして解釈した結果で解決するプロミスを返す。 */
+      .then(res => res.json())
+      .then(
+        (result) => {
+          /* APIのJSONのmessageプロパティから都度変更されるurlを取得し、書き換え */
+          setDogUrl(result.message);
+        },
+        /* コンポーネント内のバグを隠蔽しないために、ここでエラーハンドリングするのが重要 */
+        (error) => {
+          error;
+        }
+      );
   }
   return (
     <div>
