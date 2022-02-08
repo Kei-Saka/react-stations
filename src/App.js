23cc688a26894db719c0dcf4b84806e1439d22d5
kei.sakanishi@gmail.com <kei.sakanishi@gmail.com>
2022-02-08 13:41:20 +0900

    今のところは更新はしなくていいのか

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+// import * as React from 'react'
+import React, { useState } from 'react';
 import './App.css'
 
 /**
@@ -8,11 +9,12 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/maltese/n02085936_2422.jpg');
   return (
     <div>
       <header>Dogアプリ</header>
       <p>犬の画像を表示するサイトです。</p>
-      <img src='https://images.dog.ceo/breeds/maltese/n02085936_2422.jpg' />
+      <img src={dogUrl} />
     </div>
   )
 }
