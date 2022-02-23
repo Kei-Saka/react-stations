a3ee058311c9ba2822f58863f5d1e7136bb91d50
kei.sakanishi@gmail.com <kei.sakanishi@gmail.com>
2022-02-23 20:25:55 +0900

    try

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import * as React from 'react';
 import './App.css'
 import { Header } from './Header'
 import { Description } from './Description'
+import { DogListContainer } from './DogListContainer'
 
 /**
  * 
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,1 +1,26 @@
 // DO NOT DELETE
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+export const DogListContainer = () => {
+    const [breeds, setBreeds] = useState();
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/list/all")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setBreeds(result.message);
+                },
+                (error) => {
+                    error;
+                }
+            );
+    }, []);
+    return (
+        <div className='dogListContainer'>
+            <p className='dogBreeds'></p>
+        </div>
+    )
+};
+
+// 拝借
